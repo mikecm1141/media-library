2fbfc2f51e0e9295dc0d398fb5ae5879afe913bb
Mike <mikecm@gmail.com>
2019-02-22 10:11:29 -0700

    Initial Commit

---
+++ src/containers/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Header from '../common/Header';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container-fluid text-center">
+        <Header />
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired
+};
